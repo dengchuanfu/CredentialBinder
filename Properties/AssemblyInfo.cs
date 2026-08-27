be7a91122ec5aea562fcd8debb851adc039baa82
Monster <136884568+dengchuanfu@users.noreply.github.com>
2026-08-27 17:38:47 +0800

    发布 v0.0.3 第二版内部定制功能

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 
-// 发布标签与程序集版本保持同步，便于 Windows 文件属性和 GitHub Release 对应。
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyInformationalVersion("0.0.3")]
