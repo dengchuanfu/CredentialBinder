8e7b68179ec79bca780fb1aa971f1abe253cbbab
Monster <136884568+dengchuanfu@users.noreply.github.com>
2026-08-27 15:11:42 +0800

    ?? v0.0.1 ?????Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+// 发布标签与程序集版本保持同步，便于 Windows 文件属性和 GitHub Release 对应。
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
